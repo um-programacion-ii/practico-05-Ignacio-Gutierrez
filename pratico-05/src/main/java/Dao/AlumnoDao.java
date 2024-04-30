2de8a1f3c8916348f18e9c4e8c74768d2ca238d0
Ignacio Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2024-04-30 11:23:26 -0300

    Ejercicio 1- 1.

---
+++ pratico-05/src/main/java/Dao/AlumnoDao.java
@@ -0,0 +1,33 @@
+package Dao;
+
+import Entidades.Alumno;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlumnoDao {
+    private Map<Integer, Alumno> alumnos;
+
+    public AlumnoDao() {
+        this.alumnos = new HashMap<>();
+    }
+
+    public void agregarAlumno(Alumno alumno) {
+        alumnos.put(alumno.getId(), alumno);
+    }
+
+    public Alumno obtenerAlumno(int id) {
+        return alumnos.get(id);
+    }
+
+    public void actualizarAlumno(Alumno alumno) {
+        if (alumnos.containsKey(alumno.getId())) {
+            alumnos.put(alumno.getId(), alumno);
+        } else {
+            // Manejar el caso de que el alumno no exista
+        }
+    }
+
+    public void eliminarAlumno(int id) {
+        alumnos.remove(id);
+    }
+}
